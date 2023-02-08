77a99cd89785c109543924aa8278e28238111b65
Ishara Maduranga <99071775+isharamaduranga@users.noreply.github.com>
2023-02-08 21:32:12 +0530

    caching uploaded image using post mapping with Back-End..

--- Uploader-Back_End/src/main/java/lk/ijse/spring/controller/FileHandleController.java
+++ Uploader-Back_End/src/main/java/lk/ijse/spring/controller/FileHandleController.java
@@ -8,10 +8,17 @@
 
 package lk.ijse.spring.controller;
 
-
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 
-
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 
 @RestController
 @RequestMapping("/upload")
@@ -19,5 +26,17 @@ import org.springframework.web.bind.annotation.*;
 public class FileHandleController {
 
 
+
+    @PostMapping("post")
+    public ResponseEntity<String>  uploadImage(@RequestParam("imageFile") MultipartFile imageFile) throws IOException {
+
+        // Save the image to the file system or database
+        byte[] bytes = imageFile.getBytes();
+        Path path = Paths.get("D:\\upload\\" + imageFile.getOriginalFilename());
+        Files.write(path, bytes);
+
+        // Return the Success message
+        return ResponseEntity.ok("Image uploaded successfully.");
+    }
 
 }
